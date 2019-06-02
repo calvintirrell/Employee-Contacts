115fafe8613beb5f6f680aec785aff02825f8bc9
Calvin Tirrell <calvintirrell@gmail.com>
2019-06-02 13:02:46 -0700

    Initial files for basic Contacts app

---
+++ Employees.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    public class Employees
+    {
+        private const string TEXT_FILE_NAME = "Employee.txt";
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+                              //<> these are generics
+        public async static Task<ICollection<Employees>>GetEmployees()
+        {
+            //List is a wrapper around an array to be a dynamic array
+            var employees = new List<Employees>();
+            var content = await FileHelper.ReadTextFileAsync(TEXT_FILE_NAME);
+            var lines = content.Split(new char[] { '\r', '\n' });
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+                var parts = line.Split(',');
+                var employee = new Employees
+                {
+                    Name = parts[0],
+                    Title = parts[1]
+                };
+                employees.Add(employee);
+            }
+            return employees;
+
+            // Object Initialization
+            //return new Employees
+            //{
+            //    Name = "C T",
+            //    Title = "CEO"
+            //};
+        }
+
+        public static void WriteEmployee(Employees employee)
+        {
+            var employeeData = $"{employee.Name}, {employee.Title}";
+            FileHelper.WriteTextFileAsync(TEXT_FILE_NAME, employeeData);
+        }
+    }
+}
---
+++ FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace EmployeeApp
+{
+    //can't create instance because it's static
+    //all following methods must be static too
+    public static class FileHelper
+    {
+        public async static void WriteTextFileAsync(string filename, string content)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+                          //Task another name for thread (to do work)
+        public async static Task<string> ReadTextFileAsync(string filename)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await storageFolder.GetFileAsync(filename);
+            var textStream = await textFile.OpenReadAsync();
+            var textReader = new DataReader(textStream);
+            var textLength = textStream.Size;
+            await textReader.LoadAsync((uint)textLength);
+            return textReader.ReadString((uint)textLength);
+        }
+    }
+}
